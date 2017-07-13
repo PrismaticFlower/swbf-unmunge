8f1b01f615e0dbf85e46e5a0a33f091520000f31
SleepKiller <SleepKiller@users.noreply.github.com>
2017-07-13 22:14:27 +1200

    Added pod wrappers for glm types

---
+++ src/glm_pod_wrappers.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <type_traits>
+
+#define GLM_FORCE_CXX98
+#include "glm/fwd.hpp"
+#include "glm/gtc/quaternion.hpp"
+
+namespace pod {
+template<typename Type>
+struct Vec2_template {
+   Type x;
+   Type y;
+
+   operator glm::tvec2<Type>() const noexcept
+   {
+      return {x, y};
+   }
+};
+
+using Vec2 = Vec2_template<float>;
+
+static_assert(std::is_pod_v<Vec2>);
+static_assert(sizeof(Vec2) == 8);
+
+template<typename Type>
+struct Vec3_template {
+   Type x;
+   Type y;
+   Type z;
+
+   operator glm::tvec3<Type>() const noexcept
+   {
+      return {x, y, z};
+   }
+};
+
+using Vec3 = Vec3_template<float>;
+
+static_assert(std::is_pod_v<Vec3>);
+static_assert(sizeof(Vec3) == 12);
+
+template<typename Type>
+struct Vec4_template {
+   Type x;
+   Type y;
+   Type z;
+   Type w;
+
+   operator glm::tvec4<Type>() const noexcept
+   {
+      return {x, y, z};
+   }
+};
+
+using Vec4 = Vec4_template<float>;
+
+static_assert(std::is_pod_v<Vec4>);
+static_assert(sizeof(Vec4) == 16);
+
+template<typename Type>
+struct Mat3_template {
+   Type m[9];
+
+   operator glm::tmat3x3<Type>() const noexcept
+   {
+      return {m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8]};
+   }
+};
+
+using Mat3 = Mat3_template<float>;
+
+static_assert(std::is_pod_v<Mat3>);
+static_assert(sizeof(Mat3) == 36);
+
+template<typename Type>
+struct Quat_template {
+   Type x;
+   Type y;
+   Type z;
+   Type w;
+
+   operator glm::tquat<Type>() const noexcept
+   {
+      return {w, x, y, z};
+   }
+};
+
+using Quat = Quat_template<float>;
+
+static_assert(std::is_pod_v<Quat>);
+static_assert(sizeof(Quat) == 16);
+}
