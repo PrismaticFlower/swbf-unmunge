ee5736d3f1f4994c500cbe57818a2a8f82c37417
SleepKiller <SleepKiller@users.noreply.github.com>
2017-07-14 01:42:47 +1200

    Shifted handle_model and vbuf_helpers over to Ucfb_readers

--- src/vbuf_helpers.cpp
+++ src/vbuf_helpers.cpp
@@ -1,121 +1,100 @@
 
-#include "vbuf_helpers.hpp"
+#include "glm_pod_wrappers.hpp"
 #include "magic_number.hpp"
-#include "type_pun.hpp"
-
-#define GLM_FORCE_CXX98
-#include "glm/glm.hpp"
+#include "msh_builder.hpp"
+#include "ucfb_reader.hpp"
 
 #include <array>
+#include <cstdint>
 
 namespace {
 
-static_assert(std::is_standard_layout_v<glm::vec2>);
-static_assert(sizeof(glm::vec2) == 8);
-static_assert(std::is_standard_layout_v<glm::vec3>);
-static_assert(sizeof(glm::vec3) == 12);
-static_assert(std::is_standard_layout_v<glm::vec4>);
-static_assert(sizeof(glm::vec4) == 16);
-static_assert(std::is_standard_layout_v<std::array<std::uint8_t, 4>>);
-static_assert(sizeof(std::array<std::uint8_t, 4>) == 4);
+enum class Vbuf_types : std::uint32_t {
+   // xyz (position) - xyz (normal) - uv (texture coordinates)
+   xyznuv = 0x00000222,
+   // xyz (position) - xyz (normal) - rgba - uv (texture coordinates)
+   xyzncuv = 0x0000322,
+   // xyz (position) - xyz (normal) - rgba - uv (texture coordinates)
+   xyzncuv_2 = 0x000002a2,
 
-#pragma pack(push, 1)
-#pragma warning(disable : 4200)
+   // xyz (position) - skin - xyz (normal) - uv (texture coordinates)
+   xyzsknuv = 0x0000226,
 
-template<typename Entry>
-struct Vbuf_template {
-   Magic_number mn;
-   std::uint32_t size;
+   unknown_16 = 0x0000d222,
+   unknown_20 = 0x0000f226
+};
+
+struct Vbuf_info {
    std::uint32_t entry_count;
    std::uint32_t entry_size;
    Vbuf_types type;
-
-   Entry entries[];
 };
+
+static_assert(std::is_pod_v<Vbuf_info>);
+static_assert(sizeof(Vbuf_info) == 12);
 
 struct Vbuf_xyznuv_entry {
-   glm::vec3 position;
-   glm::vec3 normal;
-   glm::vec2 uv;
+   pod::Vec3 position;
+   pod::Vec3 normal;
+   pod::Vec2 uv;
 };
 
-static_assert(std::is_standard_layout_v<Vbuf_xyznuv_entry>);
+static_assert(std::is_pod_v<Vbuf_xyznuv_entry>);
 static_assert(sizeof(Vbuf_xyznuv_entry) == 32);
 
-using Vbuf_xyznuv = Vbuf_template<Vbuf_xyznuv_entry>;
-
-static_assert(std::is_standard_layout_v<Vbuf_xyznuv>);
-static_assert(sizeof(Vbuf_xyznuv) == 20);
-
 struct Vbuf_xyzncuv_entry {
-   glm::vec3 position;
-   glm::vec3 normal;
+   pod::Vec3 position;
+   pod::Vec3 normal;
    std::array<std::uint8_t, 4> rgba;
-   glm::vec2 uv;
+   pod::Vec2 uv;
 };
 
-static_assert(std::is_standard_layout_v<Vbuf_xyzncuv_entry>);
+static_assert(std::is_pod_v<Vbuf_xyzncuv_entry>);
 static_assert(sizeof(Vbuf_xyzncuv_entry) == 36);
 
-using Vbuf_xyzncuv = Vbuf_template<Vbuf_xyzncuv_entry>;
-
-static_assert(std::is_standard_layout_v<Vbuf_xyzncuv>);
-static_assert(sizeof(Vbuf_xyzncuv) == 20);
-
 struct Vbuf_xyzsknuv_entry {
-   glm::vec3 position;
+   pod::Vec3 position;
    std::array<std::uint8_t, 4> skin;
-   glm::vec3 normal;
-   glm::vec2 uv;
+   pod::Vec3 normal;
+   pod::Vec2 uv;
 };
 
-static_assert(std::is_standard_layout_v<Vbuf_xyzsknuv_entry>);
+static_assert(std::is_pod_v<Vbuf_xyzsknuv_entry>);
 static_assert(sizeof(Vbuf_xyzsknuv_entry) == 36);
-
-using Vbuf_xyzsknuv = Vbuf_template<Vbuf_xyzsknuv_entry>;
-
-static_assert(std::is_standard_layout_v<Vbuf_xyzsknuv>);
-static_assert(sizeof(Vbuf_xyzsknuv) == 20);
-
-#pragma pack(pop)
 
 glm::vec2 flip_texture_y(glm::vec2 tex_coord)
 {
    return {tex_coord.x, 1.0f - tex_coord.y};
 }
 
-void process_vbuf_impl(const Vbuf_xyznuv& vbuf, msh::Model& model)
+void process_vbuf_impl(gsl::span<const Vbuf_xyznuv_entry> entries, msh::Model& model)
 {
    model.vertices.clear();
-   model.vertices.reserve(vbuf.entry_count);
+   model.vertices.reserve(entries.size());
    model.normals.clear();
-   model.normals.reserve(vbuf.entry_count);
+   model.normals.reserve(entries.size());
    model.texture_coords.clear();
-   model.texture_coords.reserve(vbuf.entry_count);
+   model.texture_coords.reserve(entries.size());
 
-   for (std::size_t i = 0; i < vbuf.entry_count; i++) {
-      const auto& entry = vbuf.entries[i];
-
+   for (const auto& entry : entries) {
       model.vertices.push_back(entry.position);
       model.normals.push_back(entry.normal);
       model.texture_coords.push_back(flip_texture_y(entry.uv));
    }
 }
 
-void process_vbuf_impl(const Vbuf_xyzncuv& vbuf, msh::Model& model)
+void process_vbuf_impl(gsl::span<const Vbuf_xyzncuv_entry> entries, msh::Model& model)
 {
    model.vertices.clear();
-   model.vertices.reserve(vbuf.entry_count);
+   model.vertices.reserve(entries.size());
    model.normals.clear();
-   model.normals.reserve(vbuf.entry_count);
+   model.normals.reserve(entries.size());
    model.colours.clear();
-   model.colours.reserve(vbuf.entry_count);
+   model.colours.reserve(entries.size());
    model.texture_coords.clear();
-   model.texture_coords.reserve(vbuf.entry_count);
+   model.texture_coords.reserve(entries.size());
 
-   for (std::size_t i = 0; i < vbuf.entry_count; i++) {
-      const auto& entry = vbuf.entries[i];
-
+   for (const auto& entry : entries) {
       model.vertices.push_back(entry.position);
       model.normals.push_back(entry.normal);
       model.colours.push_back(entry.rgba);
@@ -123,20 +102,18 @@ void process_vbuf_impl(const Vbuf_xyzncuv& vbuf, msh::Model& model)
    }
 }
 
-void process_vbuf_impl(const Vbuf_xyzsknuv& vbuf, msh::Model& model)
+void process_vbuf_impl(gsl::span<const Vbuf_xyzsknuv_entry> entries, msh::Model& model)
 {
    model.vertices.clear();
-   model.vertices.reserve(vbuf.entry_count);
+   model.vertices.reserve(entries.size());
    model.skin.clear();
-   model.skin.reserve(vbuf.entry_count);
+   model.skin.reserve(entries.size());
    model.normals.clear();
-   model.normals.reserve(vbuf.entry_count);
+   model.normals.reserve(entries.size());
    model.texture_coords.clear();
-   model.texture_coords.reserve(vbuf.entry_count);
+   model.texture_coords.reserve(entries.size());
 
-   for (std::size_t i = 0; i < vbuf.entry_count; i++) {
-      const auto& entry = vbuf.entries[i];
-
+   for (const auto& entry : entries) {
       model.vertices.push_back(entry.position);
       model.skin.push_back(entry.skin[0]);
       model.normals.push_back(entry.normal);
@@ -145,15 +122,20 @@ void process_vbuf_impl(const Vbuf_xyzsknuv& vbuf, msh::Model& model)
 }
 }
 
-void process_vbuf(const Vbuf& vbuf, msh::Model& model)
+void process_vbuf(Ucfb_reader_strict<"VBUF"_mn> vbuf, msh::Model& model)
 {
-   if (vbuf.type == Vbuf_types::xyznuv) {
-      process_vbuf_impl(view_type_as<Vbuf_xyznuv>(vbuf), model);
+   const auto vbuf_info = vbuf.read_trivial<Vbuf_info>();
+
+   if (vbuf_info.type == Vbuf_types::xyznuv) {
+      process_vbuf_impl(vbuf.read_array<Vbuf_xyznuv_entry>(vbuf_info.entry_count), model);
    }
-   else if (vbuf.type == Vbuf_types::xyzncuv || vbuf.type == Vbuf_types::xyzncuv_2) {
-      process_vbuf_impl(view_type_as<Vbuf_xyzncuv>(vbuf), model);
+   else if (vbuf_info.type == Vbuf_types::xyzncuv ||
+            vbuf_info.type == Vbuf_types::xyzncuv_2) {
+      process_vbuf_impl(vbuf.read_array<Vbuf_xyzncuv_entry>(vbuf_info.entry_count),
+                        model);
    }
-   else if (vbuf.type == Vbuf_types::xyzsknuv) {
-      process_vbuf_impl(view_type_as<Vbuf_xyzsknuv>(vbuf), model);
+   else if (vbuf_info.type == Vbuf_types::xyzsknuv) {
+      process_vbuf_impl(vbuf.read_array<Vbuf_xyzsknuv_entry>(vbuf_info.entry_count),
+                        model);
    }
 }
--- src/vbuf_helpers.hpp
+++ src/vbuf_helpers.hpp
@@ -1,40 +1,6 @@
 #pragma once
 
-#include "byte.hpp"
-#include "magic_number.hpp"
 #include "msh_builder.hpp"
+#include "ucfb_reader.hpp"
 
-#include <cstdint>
-#include <type_traits>
-
-enum class Vbuf_types : std::uint32_t {
-   // xyz (position) - xyz (normal) - uv (texture coordinates)
-   xyznuv = 0x00000222,
-   // xyz (position) - xyz (normal) - rgba - uv (texture coordinates)
-   xyzncuv = 0x0000322,
-   // xyz (position) - xyz (normal) - rgba - uv (texture coordinates)
-   xyzncuv_2 = 0x000002a2,
-
-   // xyz (position) - skin - xyz (normal) - uv (texture coordinates)
-   xyzsknuv = 0x0000226,
-
-   unknown_16 = 0x0000d222,
-   unknown_20 = 0x0000f226
-};
-
-#pragma pack(push, 1)
-
-struct Vbuf {
-   Magic_number mn;
-   std::uint32_t size;
-   std::uint32_t entry_count;
-   std::uint32_t entry_size;
-   Vbuf_types type;
-};
-
-static_assert(std::is_standard_layout_v<Vbuf>);
-static_assert(sizeof(Vbuf) == 20);
-
-#pragma pack(pop)
-
-void process_vbuf(const Vbuf& vbuf, msh::Model& model);
+void process_vbuf(Ucfb_reader_strict<"VBUF"_mn> vbuf, msh::Model& model);
