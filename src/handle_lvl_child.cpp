1ed5805f8a6510accd977d25bdceb92e363b9582
SleepKiller <SleepKiller@users.noreply.github.com>
2017-07-17 02:28:50 +1200

    fixed a major bug in handle_lvl_child
    I forgot to consume the header of the lvl_ chunk after refactoring.
    Feels bad man.

--- src/handle_lvl_child.cpp
+++ src/handle_lvl_child.cpp
@@ -1,5 +1,4 @@
 #include "chunk_processor.hpp"
-#include "type_pun.hpp"
 
 #include "tbb/task_group.h"
 
@@ -7,6 +6,9 @@ void handle_lvl_child(Ucfb_reader lvl_child, const App_options& app_options,
                       File_saver& file_saver, tbb::task_group& tasks,
                       msh::Builders_map& msh_builders)
 {
+   lvl_child.consume(4); // lvl name hash
+   lvl_child.consume(4); // lvl size left
+
    while (lvl_child) {
       const auto child = lvl_child.read_child();
 
