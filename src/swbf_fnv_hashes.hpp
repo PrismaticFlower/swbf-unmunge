0638e95e1c1211df1c67cad4d09dcabad2c38ea2
SleepKiller <SleepKiller@users.noreply.github.com>
2017-07-24 22:36:41 +1200

    Moved fnv hash function into header

--- src/swbf_fnv_hashes.hpp
+++ src/swbf_fnv_hashes.hpp
@@ -3,4 +3,26 @@
 #include <cstdint>
 #include <string>
 
+constexpr std::uint32_t fnv_1a_hash(const std::string_view str)
+{
+   constexpr std::uint32_t FNV_prime = 16777619;
+   constexpr std::uint32_t offset_basis = 2166136261;
+
+   std::uint32_t hash = offset_basis;
+
+   for (auto c : str) {
+      c |= 0x20;
+
+      hash ^= c;
+      hash *= FNV_prime;
+   }
+
+   return hash;
+}
+
+constexpr std::uint32_t operator""_fnv(const char* str, const std::size_t length)
+{
+   return fnv_1a_hash({str, length});
+}
+
 std::string lookup_fnv_hash(std::uint32_t hash);
