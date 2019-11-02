44fb2e91874a4bdf426b1324967b89243ca9cde9
SleepKiller <SleepKiller@users.noreply.github.com>
2019-11-02 20:09:46 +1300

    stop trying to be clever with collision primitive names

--- src/handle_primitives.cpp
+++ src/handle_primitives.cpp
@@ -3,11 +3,7 @@
 #include "model_builder.hpp"
 #include "ucfb_reader.hpp"
 
-#include <ctre.hpp>
-
 namespace {
-
-constexpr static auto name_match_pattern = ctll::fixed_string{R"((p_|P_)?(-.*-_?)?(.*))"};
 
 struct Primitive_Data {
    model::Collision_primitive_type type;
@@ -17,19 +13,11 @@ struct Primitive_Data {
 static_assert(std::is_trivially_copyable_v<Primitive_Data>);
 static_assert(sizeof(Primitive_Data) == 16);
 
-auto get_primitive_name(const std::string_view full_name) -> std::string_view
-{
-   const auto [full, primitve, flags, name] = ctre::match<name_match_pattern>(full_name);
-
-   return name;
-}
-
 auto read_next_primitive(Ucfb_reader& primitives) -> model::Collision_primitive
 {
    model::Collision_primitive prim;
 
-   prim.name =
-      get_primitive_name(primitives.read_child_strict<"NAME"_mn>().read_string());
+   prim.name = primitives.read_child_strict<"NAME"_mn>().read_string();
 
    auto mask = primitives.read_child_strict_optional<"MASK"_mn>();
 
