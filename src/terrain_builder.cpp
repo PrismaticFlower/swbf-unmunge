8ad49b3135daa6e9dd4657bc0d183955c3ae8f72
PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>
2024-06-14 11:08:31 +1200

    write out basic cluster info

--- src/terrain_builder.cpp
+++ src/terrain_builder.cpp
@@ -3,6 +3,8 @@
 #include "string_helpers.hpp"
 #include "type_pun.hpp"
 
+#include <limits>
+
 #include <gsl/gsl>
 
 using namespace std::literals;
@@ -31,7 +33,7 @@ Terrain_builder::Terrain_builder(const float grid_unit_size, const float height_
    : _grid_unit_size{grid_unit_size}, _height_granularity{height_scale},
      _grid_size{grid_size}, _heightmap(grid_size * grid_size, 0i16),
      _lightmap(grid_size * grid_size, default_colour),
-     _texturemap(grid_size * grid_size, Texture_values{}),
+     _texturemap(grid_size * grid_size, Texture_values{0xff}),
      _patch_infomap((grid_size / 4) * (grid_size / 4), {Render_types::normal, 0})
 {
 }
@@ -110,6 +112,8 @@ void Terrain_builder::set_point_texture(const Point point,
 {
    Expects(texture < max_textures);
 
+   if (texture == 0) return;
+
    _texturemap[lookup_point_index(point)][texture] = value;
 }
 
@@ -130,8 +134,6 @@ void Terrain_builder::set_munge_flags(const Terrain_flags flags) noexcept
 void Terrain_builder::save(Game_version version, std::string_view name,
                            File_saver& file_saver) const
 {
-   // const Clusters_info clusters_info = make_clusters_info();
-
    std::ofstream file =
       file_saver.open_save_file("world"sv, name, ".ter"sv, std::ios::binary);
 
@@ -215,14 +217,12 @@ void Terrain_builder::save(Game_version version, std::string_view name,
    // texturemap
    write_span(file, std::span{_texturemap});
 
-   // unknown map
-   std::vector<std::uint8_t> unknown_map;
-   unknown_map.resize((_grid_size / 2) * (_grid_size / 2), 0);
+   // cluster info
+   const Clusters_info clusters_info = make_clusters_info();
 
-   write_span(file, std::span{unknown_map});
-
-   // patch infomap
-   write_span(file, std::span{_patch_infomap});
+   write_span(file, std::span{clusters_info.min_heights});
+   write_span(file, std::span{clusters_info.max_heights});
+   write_span(file, std::span{clusters_info.flags});
 }
 
 std::size_t Terrain_builder::lookup_point_index(Point point) const noexcept
@@ -241,6 +241,77 @@ std::size_t Terrain_builder::lookup_patch_index(Point patch) const noexcept
    if (patch[1] >= patch_grid_size) patch[1] %= patch_grid_size;
 
    return patch[0] + patch_grid_size * patch[1];
+}
+
+Terrain_builder::Clusters_info Terrain_builder::make_clusters_info() const noexcept
+{
+   constexpr int cluster_size = 4;
+
+   const auto clusters_length = _grid_size / cluster_size;
+
+   Clusters_info info;
+
+   info.min_heights.resize(clusters_length * clusters_length);
+   info.max_heights.resize(clusters_length * clusters_length);
+   info.flags.resize(clusters_length * clusters_length);
+
+   for (std::size_t y = 0; y < _grid_size; y += cluster_size) {
+      for (std::size_t x = 0; x < _grid_size; x += cluster_size) {
+         std::int16_t min_height = std::numeric_limits<std::int16_t>::min();
+         std::int16_t max_height = std::numeric_limits<std::int16_t>::max();
+
+         for (std::size_t local_y = 0; local_y < cluster_size; ++local_y) {
+            for (std::size_t local_x = 0; local_x < cluster_size; ++local_x) {
+               const std::int16_t height =
+                  _heightmap[lookup_point_index({x + local_x, y + local_y})];
+
+               min_height = std::min(height, min_height);
+               max_height = std::max(height, max_height);
+            }
+         }
+
+         info.min_heights[(y / cluster_size * clusters_length) + x / cluster_size] =
+            min_height;
+         info.max_heights[(y / cluster_size * clusters_length) + x / cluster_size] =
+            max_height;
+      }
+   }
+
+   for (int y = 0; y < static_cast<int>(_grid_size); y += cluster_size) {
+      for (int x = 0; x < static_cast<int>(_grid_size); x += cluster_size) {
+         std::uint32_t flags = 0;
+
+         // build texture vis mask
+         for (int local_y = -1; local_y <= cluster_size; ++local_y) {
+            for (int local_x = -1; local_x <= cluster_size; ++local_x) {
+               for (std::uint32_t i = 0; i < max_textures; ++i) {
+                  const int abs_x =
+                     std::clamp(x + local_x, 0, static_cast<int>(_grid_size) - 1);
+                  const int abs_y =
+                     std::clamp(y + local_y, 0, static_cast<int>(_grid_size) - 1);
+
+                  const std::uint8_t weight = _texturemap[lookup_point_index(
+                     {static_cast<std::size_t>(abs_x), static_cast<std::size_t>(abs_y)})]
+                                                         [i];
+
+                  flags |= (1 & (weight > 0)) << i;
+               }
+            }
+         }
+
+#if 0
+         constexpr std::uint32_t cluster_info_water_bit = 0x10000;
+
+         if (water_map[{x / cluster_size, y / cluster_size}]) {
+            flags |= cluster_info_water_bit;
+         }
+#endif
+
+         info.flags[(y / cluster_size * clusters_length) + x / cluster_size] = flags;
+      }
+   }
+
+   return info;
 }
 
 void save_void_terrain(Game_version version, std::string_view name,
